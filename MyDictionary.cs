f020963f901e2371e71df649adc8db3d22968524
verysadbird <77414505+verysadbird@users.noreply.github.com>
2021-02-07 11:04:21 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Dictionary
+{
+    class MyDictionary <TKeys, TValues>
+    {
+        TKeys[] keys;
+        TValues[] values;
+
+        public MyDictionary()
+        {
+            keys = new TKeys[0];
+            values = new TValues[0];
+        }
+
+
+        public void Add(TKeys key, TValues value)
+        {
+
+            TKeys[] tempKeys = keys;
+            TValues[] tempValues = values;
+
+            keys = new TKeys[tempKeys.Length + 1];
+            values = new TValues[tempValues.Length + 1];
+
+            for(int i=0; i<tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value; 
+
+        }
+
+        public void ToList()
+        {
+            for(int i=0; i<keys.Length; i++)
+            {
+                Console.WriteLine("Key:   " + keys[i] + "    Value:    " + values[i]);
+            }
+        }
+
+        
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<int, string> productCategories = new MyDictionary<int, string>();
+            productCategories.Add(0, "Home Textile");
+            productCategories.Add(1, "Fashion");
+            productCategories.Add(2, "Lightning");
+            productCategories.Add(3, "Furniture");
+
+            productCategories.ToList();
+
+        }
+    }
+}
